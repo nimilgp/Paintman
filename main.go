d1525467e15dd9d15c504b9843e0e48dbdf6a87d
nimil <nimilgeorgepradeepngp@gmail.com>
2024-09-22 18:34:07 +0530

    forground and background colors can be set with flags

--- main.go
+++ main.go
@@ -1,20 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
-const Reset = "\033[0m"
-const Red = "\033[31m"
-const Green = "\033[32m"
-const Yellow = "\033[33m"
-const Blue = "\033[34m"
-const Magenta = "\033[35m"
-const Cyan = "\033[36m"
-const White = "\033[97m"
+const RESET = "\033[0m"
 
 func main() {
-	contentStr := "this is a line\nthis is the second line\nfinal line"
-	r, g, b := 0, 0, 255
-	for _, char := range contentStr {
-		fmt.Printf("\033[38;2;%d;%d;%dm%c", r, g, b, char)
+	colorValueMap := map[string]int{
+		"black":   30,
+		"red":     31,
+		"green":   32,
+		"yellow":  33,
+		"blue":    34,
+		"magenta": 35,
+		"cyan":    36,
+		"white":   97,
 	}
+	var fgcolor string
+	var bgcolor string
+	var showColors bool
+	flag.StringVar(&fgcolor, "fg", "white", "select foreground color")
+	flag.StringVar(&bgcolor, "bg", "cyan", "select background color")
+	flag.BoolVar(&showColors, "colors", false, "show availible color options")
+	flag.Parse()
+
+	if showColors {
+		fmt.Println("red, green, yellow, blue, magenta, cyan, white, black")
+		return
+	}
+
+	fg, ok := colorValueMap[fgcolor]
+	if !ok {
+		fmt.Println("foreground color is invalid")
+		return
+	}
+	bg, ok := colorValueMap[bgcolor]
+	if !ok {
+		fmt.Println("background color is invalid")
+		return
+	}
+
+	contentStr := "this is a line\nthis is the second line\nfinal line"
+	fmt.Printf("\033[%dm", fg)
+	fmt.Printf("\033[%dm", bg+10)
+	fmt.Print(contentStr)
+	fmt.Printf("%s", RESET)
 }
