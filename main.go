3838176f30ac92e5a7e8a4a5c814cfdc0b0250f1
nimil <nimilgeorgepradeepngp@gmail.com>
2024-09-22 15:34:57 +0530

    changed char color with ascii escape char

--- main.go
+++ main.go
@@ -2,7 +2,19 @@ package main
 
 import "fmt"
 
+const Reset = "\033[0m"
+const Red = "\033[31m"
+const Green = "\033[32m"
+const Yellow = "\033[33m"
+const Blue = "\033[34m"
+const Magenta = "\033[35m"
+const Cyan = "\033[36m"
+const White = "\033[97m"
+
 func main() {
-	exampleStr := "this is a line\nthis is the second line\nfinal line"
-	fmt.Printf(exampleStr)
+	contentStr := "this is a line\nthis is the second line\nfinal line"
+
+	for _, char := range contentStr {
+		fmt.Printf("%s%c", Red, char)
+	}
 }
